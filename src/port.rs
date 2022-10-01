456641aacdef9e706f8c439d15f5c010a6925228
Mihai Galos <mihaigalos@gmail.com>
2022-10-01 12:27:11 +0200

    Clippy: Fix warnings

--- src/port.rs
+++ src/port.rs
@@ -1,18 +1,10 @@
 use std::net::TcpListener;
 use std::ops::Range;
 
-pub fn next_port_in_range(port_range: Range<u16>) -> Option<u16> {
-    for port in port_range {
-        if is_port_available(port) {
-            return Some(port);
-        }
-    }
-    None
+pub fn next_port_in_range(mut port_range: Range<u16>) -> Option<u16> {
+    port_range.find(|&port| is_port_available(port))
 }
 
 fn is_port_available(port: u16) -> bool {
-    match TcpListener::bind(("127.0.0.1", port)) {
-        Ok(_) => true,
-        Err(_) => false,
-    }
+    TcpListener::bind(("127.0.0.1", port)).is_ok()
 }
